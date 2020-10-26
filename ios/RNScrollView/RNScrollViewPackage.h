9101ba97e936270567079300a63247b5f48d57dd
Wayaer <wayaer@foxmail.com>
2020-10-26 15:10:14 +0800

    no message

--- ios/RNScrollView/RNScrollViewPackage.h
+++ ios/RNScrollView/RNScrollViewPackage.h
@@ -1,8 +1,8 @@
-#import <React/RCTRNScrollViewPackage.h>
+#import <React/RCTScrollViewManager.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface RNScrollViewPackage : RCTRNScrollViewPackage
+@interface RNScrollViewPackage : RCTScrollViewManager
 
 @end
 
